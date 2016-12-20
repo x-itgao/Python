3cd9a018afbad4a47cfb7e0000bc2f936a2cc38e
x-itgao <1148708206@qq.com>
2016-12-20 11:55:34 +0800

    equality

---
+++ challenge/equality.py
@@ -0,0 +1,20 @@
+# -*- coding:utf-8 -*-
+
+"""
+@author:itgao
+@file: equality.py
+@time: 2016/12/20 11:14
+"""
+
+import re
+# equality.data 数据
+# One small letter, surrounded by EXACTLY three big bodyguards on each of its sides.
+
+with open('equality.data','r') as file:
+    page_data = file.read()
+file.close()
+page_data = ''.join(page_data)
+data = re.findall(r'[^A-Z][A-Z]{3}([a-z])[A-Z]{3}[^A-Z]',page_data)
+print ''.join(data)
+
+
